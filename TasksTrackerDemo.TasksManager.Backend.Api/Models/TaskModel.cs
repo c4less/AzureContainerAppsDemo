39f5c379701aee792a66f6662916edc1b89a55a4
Charles Raj <c_4less@live.in>
2022-11-10 17:44:10 -0500

    Initial Commit

---
+++ TasksTrackerDemo.TasksManager.Backend.Api/Models/TaskModel.cs
@@ -0,0 +1,29 @@
+namespace TasksTrackerDemo.TasksManager.Backend.Api.Models;
+
+public class TaskModel
+{
+    public Guid TaskId { get; set; }
+    public string TaskName { get; set; } = string.Empty;
+    public string TaskCreatedBy { get; set; } = string.Empty;
+    public DateTime TaskCreatedOn { get; set; }
+    public DateTime TaskDueDate { get; set; }
+    public string TaskAssignedTo { get; set; } = string.Empty;
+    public bool IsCompleted { get; set; }
+    public bool IsOverDue { get; set; }
+}
+
+public class TaskAddModel
+{
+    public string TaskName { get; set; } = string.Empty;
+    public string TaskCreatedby { get; set; } = string.Empty;
+    public DateTime TaskDueDate { get; set; }
+    public string TaskAssignedTo { get; set; } = string.Empty;
+}
+
+public class TaskUpdateModel
+{
+    public Guid TaskId { get; set; }
+    public string TaskName { get; set; } = string.Empty;
+    public DateTime TaskDueDate { get; set; }
+    public string TaskAssignedTo { get; set; } = string.Empty;
+}
